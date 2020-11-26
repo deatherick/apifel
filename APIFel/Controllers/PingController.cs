d3b1300b6acd8fe94d9119086d82dd26ee49e4a4
Erick Morales <erick.sisol@gmail.com>
2020-11-25 20:24:52 -0600

    Commit inicial

---
+++ APIFel/Controllers/PingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace APIFel.Controllers
+{
+    [ApiController]
+    [Route("Ping")]
+    public class PingController : ControllerBase, IPingService
+    {
+
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Ping(string msg)
+        {
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+    }
+}
---
+++ APIFel/IPingService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ServiceModel;
+
+namespace APIFel
+{
+    [ServiceContract]
+    public interface IPingService
+    {
+
+        [OperationContract]
+        IEnumerable<WeatherForecast> Ping(string msg);
+    }
+}
