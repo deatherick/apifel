6ecfa2157322bec665e86912f2890e8a42b86bf8
Erick Morales <erick.sisol@gmail.com>
2020-11-30 11:17:31 -0600

    Cambio en el modelo de GenerarXML

--- APIFel/Interfaces/ICertificarDocumentoService.cs
+++ APIFel/Interfaces/ICertificarDocumentoService.cs
@@ -4,13 +4,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.ServiceModel;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
 
 namespace APIFel.Services
 {
     [ServiceContract]
     public interface ICertificarDocumentoService
     {
-        [OperationContract]
+        [OperationContract(Name = "CertificarDocumento")]
         Response<string> CertificarDocumento(CertificarDocumento certificarDocumento);
     }
 }
---
+++ APIFel/Interfaces/IGeneraXmlService.cs
@@ -0,0 +1,15 @@
+﻿using APIFel.Model;
+using System.ServiceModel;
+using Microsoft.AspNetCore.Mvc;
+
+namespace APIFel.Services
+{
+    [ServiceContract]
+    public interface IGenerarXmlService
+    {
+        IActionResult GeneraXMLRest(Documento documento);
+
+        [OperationContract(Name = "GeneraXML")]
+        Response<string> GeneraXMLSoap(Documento documento);
+    }
+}
